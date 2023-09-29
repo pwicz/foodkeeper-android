7a7105d166dd5653c58f089106a7b12b26754304
Piotr Politowicz <P.A.Politowicz@student.tudelft.nl>
2023-09-29 21:43:13 +1000

    Initialize repo with screen files

---
+++ app/src/main/java/com/example/foodkeeper/ui/KitchenScreen.kt
@@ -0,0 +1,9 @@
+package com.example.foodkeeper.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun KitchenScreen() {
+
+}
+
---
+++ app/src/main/java/com/example/foodkeeper/ui/ScannedScreen.kt
@@ -0,0 +1,8 @@
+package com.example.foodkeeper.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ScannedScreen() {
+
+}
