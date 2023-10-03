099cfad2d15ea1bd2d72d63fc61a84bb29450ebd
Yolandahehe <yolanda.ye@enactusmelbourne.com>
2023-10-04 09:26:50 +1100

    Add Button to the screen

--- app/src/main/java/com/example/foodkeeper/ui/ScannedScreen.kt
+++ app/src/main/java/com/example/foodkeeper/ui/ScannedScreen.kt
@@ -1,8 +1,40 @@
 package com.example.foodkeeper.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.foodkeeper.ui.theme.FoodKeeperTheme
 
 @Composable
-fun ScannedScreen() {
+fun ScannedScreen(name: String) {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ){
+            Button(
+                onClick = { /*TODO*/ },
+            ) {
+                Text(text = "Scan again")
+            }
+        }
+    }
+
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FoodKeeperTheme {
+        ScannedScreen(name = "Greeting")
+    }
 
 }
