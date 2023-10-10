35f6a6f85982daaee9edcc4b2c193d7bdd1c7cf5
alicelinhdam <linh.dam@enactusmelbourne.com>
2023-10-10 22:36:26 +1100

    Add Shopping Screen

--- app/src/main/java/com/example/foodkeeper/ui/KitchenScreen.kt
+++ app/src/main/java/com/example/foodkeeper/ui/KitchenScreen.kt
@@ -1,9 +1,12 @@
 package com.example.foodkeeper.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-
-@Composable
-fun KitchenScreen() {
-
-}
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
